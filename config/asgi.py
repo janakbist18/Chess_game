312f88169dddf1c96916f5ff7d5b2eca08e686db
janakbista <janakbist64@gmail.com>
2026-03-04 14:01:11 +0545

    ...

--- config/asgi.py
+++ config/asgi.py
@@ -1,16 +1,20 @@
 import os
-from django.core.asgi import get_asgi_application
-from channels.routing import ProtocolTypeRouter, URLRouter
-from channels.auth import AuthMiddlewareStack
-import chessapp.routing
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
 
+from django.core.asgi import get_asgi_application
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.auth import AuthMiddlewareStack
+
 django_asgi_app = get_asgi_application()
 
-application = ProtocolTypeRouter({
-    "http": django_asgi_app,
-    "websocket": AuthMiddlewareStack(
-        URLRouter(chessapp.routing.websocket_urlpatterns)
-    ),
-})
+import chessapp.routing  # <-- import AFTER django_asgi_app is created
+
+application = ProtocolTypeRouter(
+    {
+        "http": django_asgi_app,
+        "websocket": AuthMiddlewareStack(
+            URLRouter(chessapp.routing.websocket_urlpatterns)
+        ),
+    }
+)
